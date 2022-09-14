308d7fc5db9e1ce3cd57579eb40fd893ea577906
Anthony Pham <akpham95@gmail.com>
2022-09-14 14:47:20 -0500

    Problem 6-7 solved

--- lab.js
+++ lab.js
@@ -93,7 +93,9 @@ for (let i = 0; i < 10; i++) {
   Create a for-loop that runs 10 times, logging the numbers 1 through 10. Ex, on the first iteration it would log 1, on the second iteration it would log 2.
 */
 
-//CODE HERE
+for (let i = 0; i < 10; i++) {
+  console.log("iteration",i)
+}
 
 ////////// PROBLEM 7 //////////
 
@@ -101,7 +103,9 @@ for (let i = 0; i < 10; i++) {
   Create a for-loop that logs the numbers 0 through 10 backwards. Ex, on the first iteration it would log 10, on the second iteration it would log 9.
 */
 
-//CODE HERE
+for (let i = 10; i > 0; i--) {
+  console.log("iteration",i)
+}
 
 
 ////////// PROBLEM 8 //////////
